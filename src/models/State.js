8456be4069c9cfaf407c5aceeed6bb4fe4db413e
Ismail bagayoko <bagayoko.ismail@gmail.com>
2019-04-25 01:59:33 -0300

    Commentaire Classes State Resource

--- src/models/State.js
+++ src/models/State.js
@@ -1,4 +1,14 @@
+/**
+ * La classe State definit l'etat d'une tache a un instant donnee
+ */
 export class State {
+    /**
+     * 
+     * @param {int} x La coordonnne en x ou dessine l'etat
+     * @param {int} y la coordonnne en y ou dessine l'etat
+     * @param {string} color La couleur de remplisage de l'etat
+     * @param {string} name Le nom de l'etat optionnel
+     */
     constructor(x,y,color=0, name=""){
         this.x=x
         this.y=y
@@ -6,13 +16,18 @@ export class State {
         this.name=name
         this.wh = 30
     }
+    /**
+     * Affiche l'etat avec la couleur et a la position specifiée
+     */
     show(){
         fill(this.color); // Use color variable 'c' as fill color
         rect(this.x, this.y-this.wh, this.wh, this.wh); // Draw rectangle
         this.showName()
-
-        
     }
+    /**
+     * 
+     * @param {int} size La taile de polie pour ecrire le nom de l'etat
+     */
     showName(size=24){
         fill(0)
         let TextSize = size
