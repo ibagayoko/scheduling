45fcf0cd657770cbb7f52643b4b0e9de195e5b68
Ismail bagayoko <bagayoko.ismail@gmail.com>
2019-04-25 00:57:41 -0300

    arrangement

--- src/AbstractServer.py
+++
@@ -1,8 +0,0 @@
-class AbstractServer(object):
-    def __init__(self, budget):
-        pass
-    def __call__(self, inputs):
-        self.call(inputs)
-
-    def call(self, inputs=None):
-        raise NotImplementedError
--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# Source
-# http://beru.univ-brest.fr/~singhoff/cheddar/publications/muller14.pdf
-from .AbstractServer import AbstractServer
-from .DeferrableServer import DeferrableServer
-from .PollingServer import PollingServer
-from .SporadicServer import SporadicServer
